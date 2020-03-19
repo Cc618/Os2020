9b9b9340b0b27906c23828042ba2f5ef14b29b1f
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-19 18:22:19 +0100

    Updated boot

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -3,15 +3,17 @@
 [bits 16]
 [org 0x7C00]
 
-%include "constants.asm"
+%include "constants.inc"
 
 bootloader_begin:
 
 %include "boot.asm"
 
 ; Pad and add the magic number
-times 0x200 - 2 - (bootloader_begin - $$)
-dw 0x55AA
+times 0x200 - 2 - ($ - bootloader_begin) db 0
+
+; Boot magic number
+dw 0xAA55
 
 ; We are at offset 0x7E00
 %include "loader.asm"
--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -1,1 +1,1 @@
-db 'LOADER'
+nop
