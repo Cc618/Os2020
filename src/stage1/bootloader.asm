6079865b8078a9bf41bb9c0f151fa95b7de3fde7
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-20 09:14:46 +0100

    Renamed linker script for stage2

--- src/stage1/bootloader.asm
+++ src/stage1/bootloader.asm
@@ -9,14 +9,11 @@ bootloader_begin:
 
 %include "boot.asm"
 
+; We are at offset 0x7E00
+%include "loader.asm"
+
 ; Pad and add the magic number
 times 0x200 - 2 - ($ - bootloader_begin) db 0
 
 ; Boot magic number
 dw 0xAA55
-
-; We are at offset 0x7E00
-%include "loader.asm"
-
-; Terminate sector
-align 0x200
