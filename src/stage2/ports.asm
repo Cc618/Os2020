74ae9dbd2847a4bc46f61c70c0cb418f5148da04
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-21 19:06:04 +0100

    Added ports in stage2

---
+++ src/stage2/ports.asm
@@ -0,0 +1,125 @@
+; Outputs a byte from a port
+global outb
+outb:
+	push ebp
+	mov ebp, esp
+
+	push ax
+	push dx
+
+	; dx = port, al = value
+	mov dx, word [ebp + 12]
+	mov al, byte [ebp + 8]
+	out dx, al
+
+	pop dx
+	pop ax
+
+	leave
+	ret
+
+
+; Outputs a word from a port
+global outw
+outw:
+	push ebp
+	mov ebp, esp
+
+	push ax
+	push dx
+
+	; dx = port, ax = value
+	mov dx, word [ebp + 12]
+	mov ax, word [ebp + 8]
+	out dx, ax
+
+	pop dx
+	pop ax
+
+	leave
+	ret
+
+
+; Receives a byte from a port
+global inb
+inb:
+	push ebp
+	mov ebp, esp
+
+	push dx
+
+	; dx = port, al = value
+	mov dx, word [ebp + 8]
+	in al, dx
+
+	pop dx
+
+	leave
+	ret
+
+
+; Receives a byte from a port
+global inb
+inb:
+	push ebp
+	mov ebp, esp
+
+	push dx
+
+	; dx = port, al = value
+	mov dx, word [ebp + 8]
+	in al, dx
+
+	pop dx
+
+	leave
+	ret
+
+
+
+
+
+
+
+
+
+
+
+global loadAsm
+loadAsm:
+   mov     dx,1f6h         ;Drive and head port
+   mov     al,0a0h         ;Drive 0, head 0
+   out     dx,al
+
+   mov     dx,1f2h         ;Sector count port
+   mov     al,1            ;Read one sector
+   out     dx,al
+
+   mov     dx,1f3h         ;Sector number port
+   mov     al, 1  ;;;          ;Read sector one
+   out     dx,al
+
+   mov     dx,1f4h         ;Cylinder low port
+   mov     al,0            ;Cylinder 0
+   out     dx,al
+
+   mov     dx,1f5h         ;Cylinder high port
+   mov     al,0            ;The rest of the cylinder 0
+   out     dx,al
+
+   mov     dx,1f7h         ;Command port
+   mov     al,20h          ;Read with retry.
+   out     dx,al
+
+still_going:
+   in      al,dx
+   test    al,8            ;This means the sector buffer requires
+            ;servicing.
+   jz      still_going     ;Don't continue until the sector buffer
+            ;is ready.
+
+   mov     cx, 512 / 2        ;One sector /2
+   mov     di, 0xB8000
+   mov     dx, 1f0h         ;Data port - data comes in and out of here.
+   rep     insw
+
---
+++ src/stage2/ports.h
@@ -0,0 +1,5 @@
+#pragma once
+
+extern void outb(char data, short port);
+
+extern char inb(short port);
