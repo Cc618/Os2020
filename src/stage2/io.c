456cb03bbdabf3a206a80977b5b9499cee26e09d
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-23 16:20:25 +0100

    stage2/io: Renamed ATA ports constants

--- src/stage2/io.c
+++ src/stage2/io.c
@@ -11,20 +11,20 @@
 #define FMT_DEFAULT 0x07
 #define FMT_BLUE_SCREEN 0x9F
 
-// Ports
+// Ports for VGA
 #define VGA_SELECT 0x3D4
 #define VGA_DATA 0x3D5
 
-// TODO : ren
-#define DATA 0x1F0
-#define ERROR (DATA + 1)
-#define DEVICE (DATA + 6)
-#define SECTOR_COUNT (DATA + 2)
-#define LBA_LOW (DATA + 3)
-#define LBA_MID (DATA + 4)
-#define LBA_HI (DATA + 5)
-#define CMD (DATA + 7)
-#define CTRL (DATA + 0x206)
+// Ports for ATA
+#define ATA_DATA 0x1F0
+#define ATA_ERROR (ATA_DATA + 1)
+#define ATA_DEVICE (ATA_DATA + 6)
+#define ATA_SECTOR_COUNT (ATA_DATA + 2)
+#define ATA_LBA_LOW (ATA_DATA + 3)
+#define ATA_LBA_MID (ATA_DATA + 4)
+#define ATA_LBA_HI (ATA_DATA + 5)
+#define ATA_CMD (ATA_DATA + 7)
+#define ATA_CTRL (ATA_DATA + 0x206)
 
 unsigned int cursorPos = 0;
 
@@ -97,38 +97,38 @@ void halt()
 void identifyDisk()
 {
     // TODO :
-    outb(0xA0, DEVICE);
-    outb(0x0, CTRL);
+    outb(0xA0, ATA_DEVICE);
+    outb(0x0, ATA_CTRL);
 
-    outb(0xA0, DEVICE);
-    unsigned char status = inb(CMD);
+    outb(0xA0, ATA_DEVICE);
+    unsigned char status = inb(ATA_CMD);
     if (status == 0xFF)
         fatal("DISK"); // TODO :
 
     // Identify command
-    outb(0xA0, DEVICE);
-    outb(0, SECTOR_COUNT);
-    outb(0, LBA_LOW);
-    outb(0, LBA_MID);
-    outb(0, LBA_HI);
-    outb(0xEC, CMD);
+    outb(0xA0, ATA_DEVICE);
+    outb(0, ATA_SECTOR_COUNT);
+    outb(0, ATA_LBA_LOW);
+    outb(0, ATA_LBA_MID);
+    outb(0, ATA_LBA_HI);
+    outb(0xEC, ATA_CMD);
 
     // Test exists
-    if (inb(CMD) == 0)
+    if (inb(ATA_CMD) == 0)
         fatal("Drive doesn't exist");
 
     // Test SATA drive
-    if (((status = inb(CMD)) & 0x80) != 0)
-        if (inb(LBA_MID) != 0 || inb(LBA_HI) != 0)
+    if (((status = inb(ATA_CMD)) & 0x80) != 0)
+        if (inb(ATA_LBA_MID) != 0 || inb(ATA_LBA_HI) != 0)
             fatal("Not a SATA drive");
 
     // Poll
-    while ((status = inb(CMD) & 8) == 0 && (status & 1) == 0);
+    while ((status = inb(ATA_CMD) & 8) == 0 && (status & 1) == 0);
 
     // Receive information
     short data[256];
     for(int i = 0; i < 256; i++)
-        data[i] = inw(DATA);
+        data[i] = inw(ATA_DATA);
 
     int handle48 = data[83];
     // Addressable sectors
@@ -140,18 +140,18 @@ void readDisk(int sector, void *dst)
     int count = 20;
 
     // Read 1 sector number "sector" command
-    outb(0xE0 | ((sector & 0x0F000000) >> 24), DEVICE);
-    outb(0, ERROR);
-    outb(1, SECTOR_COUNT);
-    outb(sector & 0xFF, LBA_LOW);
-    outb((sector & 0xFF00) >> 8, LBA_MID);
-    outb((sector & 0xFF0000) >> 16, LBA_HI);
-    outb(0x20, CMD);
+    outb(0xE0 | ((sector & 0x0F000000) >> 24), ATA_DEVICE);
+    outb(0, ATA_ERROR);
+    outb(1, ATA_SECTOR_COUNT);
+    outb(sector & 0xFF, ATA_LBA_LOW);
+    outb((sector & 0xFF00) >> 8, ATA_LBA_MID);
+    outb((sector & 0xFF0000) >> 16, ATA_LBA_HI);
+    outb(0x20, ATA_CMD);
 
-    char status = inb(CMD);
-    // unsigned char status = inb(CMD);
+    char status = inb(ATA_CMD);
+    // unsigned char status = inb(ATA_CMD);
     while((status & 0x80) == 0x80 && (status & 0x01) == 0)
-        status = inb(CMD);
+        status = inb(ATA_CMD);
 
     if (status & 0x01)
         fatal("Disk can't be read");
@@ -160,7 +160,7 @@ void readDisk(int sector, void *dst)
     for(int i = 0; i < 256; ++i)
     {
         // Receive word
-        short word = inw(DATA);
+        short word = inw(ATA_DATA);
 
         // Send to destination
         ((short*)dst)[i] = word;
