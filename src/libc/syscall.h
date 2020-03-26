da8c2fb325bdfd256a56cc84b68a587b76ef3f1a
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-26 10:27:18 +0100

    Added libc syscall.h

---
+++ src/libc/syscall.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Syscall implementation in libc
+
+#include <stdint.h>
+
+// To call a syscall without setting parameters to 0
+#define SYSC0(ID)                           syscall(ID, 0, 0, 0, 0)
+#define SYSC1(ID, arg1)                     syscall(ID, arg1, 0, 0, 0)
+#define SYSC2(ID, arg1, arg2)               syscall(ID, arg1, arg2, 0, 0)
+#define SYSC3(ID, arg1, arg2, arg3)         syscall(ID, arg1, arg2, arg3, 0)
+#define SYSC4(ID, arg1, arg2, arg3, arg4)   syscall(ID, arg1, arg2, arg3, arg4)
+
+// Syscalls ids
+// See doc/syscalls.md for specs
+#define SYS_FATAL       0x02
+#define SYS_PUTC        0x10
+
+// To raise a syscall
+void syscall(uint32_t id, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4);
