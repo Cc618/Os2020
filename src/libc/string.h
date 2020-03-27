8518e44ac4649a2bcc391ea84822d09049165c35
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-27 18:56:18 +0100

    Added libc/string.h

---
+++ src/libc/string.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stddef.h>
+
+// --- Memory --- //
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
---
+++ src/libc/string_mem.c
@@ -0,0 +1,41 @@
+#include <string.h>
+
+#include <stdint.h>
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    while (n != 0)
+    {
+        --n;
+
+        // Set memory
+        ((char*)dest)[n] = ((const char*)src)[n];
+    }
+
+    return dest;
+}
+
+void* memmove(void *dest, const void *src, size_t count)
+{
+    if (count == 0)
+        return NULL;
+
+    if (dest > src)
+    {
+        size_t i = count;
+
+        while (1)
+        {
+            --i;
+
+            ((uint8_t*) dest)[i] = ((uint8_t*) src)[i];
+
+            if (i == 0)
+                break;
+        }
+    }
+    else for (size_t i = 0; i < count; ++i)
+        ((uint8_t*) dest)[i] = ((uint8_t*) src)[i];
+
+    return dest;
+}
