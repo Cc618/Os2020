8777f05c129155bc82bd4ecade2ad8babd95fde7
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-03-29 15:55:41 +0200

    Removed useless WIP files

--- wip/libc/_stdlib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <_stddef.h>
-
-void _free(void *ptr);
-
-void *_malloc(size_t size);
--- wip/libc/sys/syscall.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// This file is not supposed to be in libc but is useful for several libc functions
-
-// TODO : extern int syscall(int id, ...);
