a2ec3100c4d917694fbf2d471a9f1a0ac40c3abf
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-04-01 14:49:12 +0200

    Added stdinPut / stdinGet

--- src/libc/_libc.h
+++ src/libc/_libc.h
@@ -5,9 +5,18 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// --- Init --- //
 // Used to init the library
 // Call this on app entry
 void __libc_init();
 
+// --- stdin --- //
 // When data comes to stdin this function is called
 void __libc_stdinCallback(void *stdinStream, uint8_t *buf, size_t n);
+
+// Adds a new char to stdin's buffer
+void __libc_stdinPut(char c);
+
+// Pops the first char of stdin's buffer
+char __libc_stdinGet();
+
--- src/libc/stdio_stdin.c
+++ src/libc/stdio_stdin.c
@@ -8,14 +8,45 @@
 
 // The buffer which gathers all
 // chars before an input
+// This buffer is used like a static queue
 static char stdinBuffer[STDIN_BUFFER_SIZE];
 
-// End of stdinBuffer
-size_t stdinBufferIndex = 0;
+// Bounds of stdinBuffer
+size_t stdinBufferStart = 0;
+size_t stdinBufferEnd = 0;
 
-// This function is defined in _libc.h
 // When the user presses a key, the key
 // is sent here after being displayed
-void __libc_stdinCallback(void *stdinStream, uint8_t *buf, size_t n)
+// The size is always one
+void __libc_stdinCallback(__attribute__((unused)) void *stdinStream, uint8_t *buf, __attribute__((unused)) size_t n)
 {
+    __libc_stdinPut((char)*buf);
+}
+
+
+// Adds a new char to stdin's buffer
+void __libc_stdinPut(char c)
+{
+    // Add
+    stdinBuffer[stdinBufferEnd] = c;
+    ++stdinBufferEnd;
+
+    // Prevent overflows
+    if (stdinBufferEnd >= STDIN_BUFFER_SIZE)
+        stdinBufferEnd = 0;
+}
+
+// Pops the first char of stdin's buffer
+// !!! Unsafe, doesn't guarantee that there
+// !!! is char to get from stdin
+char __libc_stdinGet()
+{
+    char c = stdinBuffer[stdinBufferStart];
+    ++stdinBufferStart;
+
+    // Prevent overflows
+    if (stdinBufferStart >= STDIN_BUFFER_SIZE)
+        stdinBufferStart = 0;
+
+    return c;
 }
