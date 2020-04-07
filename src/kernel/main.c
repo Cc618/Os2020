978d8e41176ed5459dba58c49c8768c9ce820c7a
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-04-07 13:23:27 +0200

    Cleaned main

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,16 +1,10 @@
-#include "drivers/screen.h"
-#include "drivers/console.h"
-#include "syscalls/syscalls.h"
-#include "io/stdstream.h"
 #include "int/interrupts.h"
+#include "drivers/console.h"
 #include "apps/shell.h"
+#include "apps/app.h"
 
-#include "syscall.h"
 #include "_libc.h"
 #include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <stdlib.h>
 
 // Entry from stage2
 void main()
