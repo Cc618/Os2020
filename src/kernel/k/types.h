d22752a2560ea522e61d8f96124cf8704ef3f21e
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-04-11 09:57:39 +0200

    Added k headers (types)

---
+++ src/kernel/k/types.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// System types
+// TODO : Include this file instead of libc ones in kernel
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int8_t      i8;
+typedef int16_t     i16;
+typedef int32_t     i32;
+
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
