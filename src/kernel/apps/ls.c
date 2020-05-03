bd4f3c70fed62593893a9406b6366d7490f43b8c
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-05-03 10:08:58 +0200

    apps: Added ls (only cwd or 1 directory)

--- src/kernel/apps/ls.c
+++ src/kernel/apps/ls.c
@@ -1,12 +1,20 @@
 #include <k/syscalls.h>
+#include <stdio.h>
 
 int lsMain(int argc, char **argv)
 {
     // TODO : Parse args
+    const char *dir = argc == 1 ? "." : argv[1];
 
     // ls
     size_t n;
-    char **children = ls(argv[1], &n);
+    char **children = ls(dir, &n);
+
+    if (children == NULL)
+    {
+        fprintf(stderr, "ls: cannot access '%s': No such file or directory\n", dir);
+        return 1;
+    }
 
     for (size_t i = 0; i < n; i++)
     {
