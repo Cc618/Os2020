cfc84554ace470a32ad84399187204564abccd0c
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-05-04 17:59:35 +0200

    apps: Updated cat for files

--- src/kernel/apps/cat.c
+++ src/kernel/apps/cat.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+#include <k/syscalls.h>
 
 #define CAT_STDIN_BUFFER_SIZE 512
+
+// Prints the content of the file
+// * Returns whether there is no error
+static bool printFile(const char *path)
+{
+    FInfo *info = finfo(path);
+    FILE *f = fopen(path, "r");
+
+    if (!f || !info)
+    {
+        if (finfo)
+            free(finfo);
+
+        fprintf(stderr, "File '%s' can't be opened\n", path);
+        return false;
+    }
+
+    char *buf = malloc(info->size);
+
+    fread(buf, 1, info->size, f);
+    fwrite(buf, 1, info->size, stdout);
+
+    free(buf);
+    free(info);
+    fclose(f);
+
+    return true;
+}
 
 int cat(int argc, char **argv)
 {
@@ -15,12 +45,14 @@ int cat(int argc, char **argv)
 
             printf("%s", buf);
         }
-
-        return 0;
     }
     else
     {
-        puts("Cat doesn't work with files now");
-        return -1;
+        size_t i = 1;
+        for ( ; i < argc && printFile(argv[i]); ++i);
+
+        return i == argc ? 0 : -1;
     }
+
+    return 0;
 }
